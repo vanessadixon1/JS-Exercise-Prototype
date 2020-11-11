a3cd1423521136cadd726f4b488870426bd9d587
Vanessa Dixon <vanessazdixon@gmail.com>
2020-11-11 16:59:05 -0500

    completed mvp and some stretch goals

--- index.js
+++ index.js
@@ -89,6 +89,12 @@ Car.prototype.fill = function(gallons) {
   this.tank += gallons;
 }
 
+Car.prototype.drive = function(drive) {
+  this.odometer+=drive;
+  if(this.milesPerGallon > 0) {
+    this.tank -= this.tank;
+  } 
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -96,9 +102,16 @@ Car.prototype.fill = function(gallons) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
 }
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
+}
+
 
 /* 
   TASK 4
